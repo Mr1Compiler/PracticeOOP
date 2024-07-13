e2588ff043ff955538ce669892476f900b7ed6e2
Ayman H <Ayman.H@gmail.com>
2024-07-13 19:18:47 +0300

    Make all code in Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,44 @@
 
 namespace Practice
 {
+    class clsA
+    {
+        public int x1 = 10;
+        private int x2 = 20;
+        protected int x3 = 30;
+
+        public int fun1()
+        {
+            return 100;
+        }
+
+        private int fun2()
+        {
+            return 200;
+        }
+
+        protected int fun3()
+        {
+            return 300;
+        }
+    }
+
+
+    class clsB : clsA
+    {
+
+        // because it's protected so by inheritance clsA we can only access to public and protected
+        // data members and functions;
+        public int fun4()
+        {
+            return x1 + x3;
+        }
+
+        public int fun5()
+        {
+            return fun1() + fun3();
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
