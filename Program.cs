ec37e5d22253a0fe1ec837e92515934c71ba484d
Ayman H <Ayman.H@gmail.com>
2024-07-13 17:06:12 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Practice
+{
+    class clsPerson
+    {
+        public string FirstName;
+        public string LastName;
+
+        public string FullName()
+        {
+            return FirstName + ' ' + LastName;
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create object from class
+            clsPerson Person1 = new clsPerson();
+
+            Person1.FirstName = "Ayman";
+            Person1.LastName = "Asal";
+
+            Console.WriteLine(Person1.FullName());
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
