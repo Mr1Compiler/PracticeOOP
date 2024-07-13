12c127c8822f25b849593693d73aaaed3b2f62e5
Ayman H <Ayman.H@gmail.com>
2024-07-13 19:24:03 +0300

    Empty Program.cs

--- Program.cs
+++ Program.cs
@@ -3,43 +3,11 @@
 
 namespace Practice
 {
-    class clsPerson
-    {
-        public string FirstName;
-        public string LastName;
-
-        public string FullName()
-        {
-            return FirstName + ' ' + LastName;
-        }
-
-    }
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Create object from class
-            clsPerson Person1 = new clsPerson();
-
-            Person1.FirstName = "Ayman";
-            Person1.LastName = "Asal";
-
-            Console.WriteLine(Person1.FullName());
-
-
-
-            clsPerson Person2 = new clsPerson();
-
-            Person2.FirstName = "Emma";
-            Person2.LastName = "Jon"; 
-
-            Console.WriteLine(Person2.FullName());
-
-
             Console.ReadKey();
-
-
         }
     }
 }
