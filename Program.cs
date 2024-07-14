138cf20acedf370eb34f4eafb3e2371148a2a0f1
Ayman H <Ayman.H@gmail.com>
2024-07-15 01:58:29 +0300

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -3,11 +3,50 @@
 
 namespace Practice
 {
-    internal class Program
+
+    class clsEmployee
     {
-        static void Main(string[] args)
+        private int _ID;
+        private string _name;
+
+        public int ID
         {
-            Console.ReadKey();
+            get
+            {
+                return _ID;
+            }
+            set
+            {
+                _ID = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                clsEmployee Employee1 = new clsEmployee();
+
+                Employee1.ID = 4;
+                Employee1.Name = "Ayman";
+
+                Console.WriteLine($"Employee ID = {Employee1.ID}");
+                Console.WriteLine($"Employee Name = {Employee1.Name}");
+
+
+                Console.ReadKey();
+            }
         }
     }
 }
