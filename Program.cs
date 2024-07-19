39059976143c889c74b997ffb9f43f4fd053f7b1
Ayman H <aymen9255@gmail.com>
2024-07-19 13:11:48 +0300

    Building parameter-less constructor

--- Program.cs
+++ Program.cs
@@ -3,10 +3,30 @@
 
 namespace Practice
 {
-    internal class Program
+    class clsPerson
     {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public clsPerson()
+        {
+            ID = -1;
+            Name = "Empty";
+            Age = 0;
+        }
+
+    }
+
+    internal class Program
+    { 
         static void Main(string[] args)
         {
+            clsPerson Person1 = new clsPerson();
+
+            Console.WriteLine($"ID = {Person1.ID} \nName = {Person1.Name} \nAge = {Person1.Age}");
+
             Console.ReadKey();
         }
     }
